487e3cd496549e272af3d430e7a8fbf45ff13fa7
DavidMartinezFlores <dmarflo526@g.educaand.es>
2024-02-18 17:48:53 +0100

    UPDATE CONFIGURATED EMPTY PORJECT WITH YAML

---
+++ src/main/java/es/iesjandula/reaktor/network_server/NetworkServerApplication.java
@@ -0,0 +1,19 @@
+package es.iesjandula.reaktor.network_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkServerApplication
+{
+
+	/**
+	 * Method main
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		SpringApplication.run(NetworkServerApplication.class, args);
+	}
+
+}
