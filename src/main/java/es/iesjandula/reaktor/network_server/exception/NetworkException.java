c0626140c6c1ff6fee1682d23fbc90ef1fa41423
PabloRuiz1312 <pruican1312@g.educaand.es>
2024-02-19 23:08:46 +0100

    Creacion de la excepcion del proyecto aprobada por el lider

---
+++ src/main/java/es/iesjandula/reaktor/network_server/exception/NetworkException.java
@@ -0,0 +1,63 @@
+package es.iesjandula.reaktor.network_server.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NetworkException extends Exception 
+{
+	/**Codigo de error */
+	private int codigo;
+	
+	/**Breve descripcion del error */
+	private String mensaje;
+	
+	/**Causa del error */
+	private Exception exception;
+	
+	/**
+	 * Constructor que crea la excepcion usando un codigo, un mensaje y una causa
+	 * @param codigo
+	 * @param mensaje
+	 * @param exception
+	 */
+	public NetworkException(int codigo,String mensaje,Exception exception)
+	{
+		super(mensaje,exception);
+		this.mensaje = mensaje;
+		this.codigo = codigo;
+		this.exception = exception;
+	}
+	/**
+	 * Constructor que crea la excepcion usando un codigo y un mensaje 
+	 * @param codigo
+	 * @param mensaje
+	 * @param exception
+	 */
+	public NetworkException(int codigo,String mensaje)
+	{
+		super(mensaje);
+		this.mensaje = mensaje;
+		this.codigo = codigo;
+		this.exception = null;
+	}
+	
+	/**
+	 * Metodo que devuelve la causa del error en un mapa
+	 * @return mapa con la informacion del error
+	 */
+	public Map<String,Object> getBodyException()
+	{
+		Map<String,Object> body = new HashMap<String,Object>();
+		
+		body.put("code", this.codigo);
+		body.put("message", this.mensaje);
+		if(this.exception!=null)
+		{
+			body.put("causa", exception.getMessage());
+		}
+		
+		return body;
+		
+		
+	}
+}
