ccc8f8ad9d596b22e17fb53adf55144df2b936fa
PabloRuiz1312 <pruican1312@g.educaand.es>
2024-02-19 10:55:54 +0100

    Creacion de las clases rest y parser como plantilla para la tarea 3

---
+++ src/main/java/es/iesjandula/reaktor/network_server/parser/Parser.java
@@ -0,0 +1,14 @@
+package es.iesjandula.reaktor.network_server.parser;
+
+
+public class Parser 
+{
+	/**
+	 * Pendiente implementacion del codigo
+	 * @author Pablo Ruiz Canovas
+	 */
+	public void parseIpConfig()
+	{
+		
+	}
+}
---
+++ src/main/java/es/iesjandula/reaktor/network_server/rest/NetworkRestApplication.java
@@ -0,0 +1,14 @@
+package es.iesjandula.reaktor.network_server.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping( value = "/net", produces="application/json")
+/**
+ * Sutil cambialo a tu manera luego esto es solo la plantilla para la tarea
+ */
+public class NetworkRestApplication 
+{
+	
+}
