1a6036664072eb8006f5117ea25f08e2b7408dbf
PabloRuiz1312 <pruican1312@g.educaand.es>
2024-02-20 11:46:11 +0100

    Actualizacion de la tarea 3 a las especificaciones del lider

--- src/main/java/es/iesjandula/reaktor/network_server/parser/Parser.java
+++ src/main/java/es/iesjandula/reaktor/network_server/parser/Parser.java
@@ -18,17 +18,18 @@ public class Parser
 	/**Logger de la clase */
 	private static Logger log = LogManager.getLogger();
 	/**
-	 * Pendiente implementacion del codigo
-	 * @author Pablo Ruiz Canovas
+	 * Metodo que recibe el contenido del comando ipconfig y lo parsea a un mapa de <String,List<String>>
+	 * @param content contenido del comando
+	 * @return mapa con la informacion de cada adaptador
+	 * @throws NetworkException
 	 */
-	public Map<String,List<String>> parseIpConfig() throws NetworkException
+	public Map<String,List<String>> parseIpConfig(String content) throws NetworkException
 	{
 		//Declaracion del mapa
 		Map<String,List<String>> map = new HashMap<String,List<String>>();
 		//Cargado de datos del comando
-		String contenido = this.contentCommand();
 		//Separamos el contenido por \n
-		String [] splitContenido = contenido.split("\n");
+		String [] splitContenido = content.split("\n");
 		
 		//Creacion de la lista de datos de cada adaptador 
 		List<String> datosConf = new LinkedList<String>();
@@ -63,61 +64,4 @@ public class Parser
 		
 	}
 	
-	/**
-	 * Metodo que ejecuta el comando ipconfig y guarda todo el resultado de la ejecucion
-	 * @return resultado de la ejecucion en formato string
-	 * @throws NetworkException
-	 */
-	private String contentCommand() throws NetworkException
-	{
-		//Declaracion de variables
-		String comando = "ipconfig";
-		String contenido = "";
-		BufferedReader reader = null;
-		try
-		{
-			//Constructor del proceso en el que se llama a la cmd y se ejecuta el comando
-			ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c",comando);
-			
-			//Creacion del proceso
-			Process proceso = builder.start();
-			
-			//Ejecucion del proceso
-			reader = new BufferedReader(new InputStreamReader(proceso.getInputStream()));
-			
-			//Lecutra y cargado de datos del proceso
-			String linea = reader.readLine();
-			while(linea!=null)
-			{
-				//Nos interesa guardar las lineas que contengan datos por lo que
-				//Se omiten las vacias
-				if(!linea.isEmpty())
-				{
-					contenido+=linea+"\n";
-				}
-				linea = reader.readLine();
-			}
-			
-		}
-		catch(IOException ex)
-		{
-			log.error("Error leyendo el resultado del comando",ex);
-			throw new NetworkException(500,"Error al leer el resultado del comando ipconfig",ex);
-		}
-		finally
-		{
-			try
-			{
-				if(reader!=null)
-				{
-					reader.close();
-				}	
-			}
-			catch(IOException ex)
-			{
-				log.error("Error cerrando el flujo de entrada",ex);
-			}
-		}
-		return contenido;
-	}
 }
