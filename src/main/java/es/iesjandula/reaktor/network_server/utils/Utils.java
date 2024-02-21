50e2ca2a23e7789570ca736209d2e94e706fabf1
unknown <mriogut600@g.educaand.es>
2024-02-21 13:30:23 +0100

    Task 4: Translate Ip and Subnet Mask to a Network Address

---
+++ src/main/java/es/iesjandula/reaktor/network_server/utils/Utils.java
@@ -0,0 +1,57 @@
+package es.iesjandula.reaktor.network_server.utils;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import es.iesjandula.reaktor.network_server.exception.NetworkException;
+
+public class Utils
+{
+
+	public static String getNetworkAddress(String ipAddress, String subnetMask) throws NetworkException
+	{
+		// Convert the IP and subnet mask strings to InetAddress objects
+		
+		InetAddress ip;
+		try
+		{
+			ip = InetAddress.getByName(ipAddress);
+			InetAddress mask = InetAddress.getByName(subnetMask);
+
+			// Get the bytes of the IP and subnet mask addresses
+			byte[] ipBytes = ip.getAddress();
+			byte[] maskBytes = mask.getAddress();
+
+			// Perform the logical AND operation between the IP and subnet mask bytes
+			byte[] networkBytes = new byte[ipBytes.length];
+			for (int i = 0; i < ipBytes.length; i++)
+			{
+				networkBytes[i] = (byte) (ipBytes[i] & maskBytes[i]);
+			}
+
+			// Create an InetAddress object with the resulting bytes
+			InetAddress networkAddress = InetAddress.getByAddress(networkBytes);
+
+			// Calculate the subnet mask prefix length
+			int prefixLength = 0;
+			for (byte b : maskBytes)
+			{
+				for (int i = 7; i >= 0; i--)
+				{
+					if ((b & (1 << i)) != 0)
+					{
+						prefixLength++;
+					} else
+					{
+						break;
+					}
+				}
+			}
+			// Return the network address and subnet mask prefix in string format
+			return networkAddress.getHostAddress() + "/" + Integer.toString(prefixLength);
+		} catch (UnknownHostException e)
+		{
+			throw new NetworkException(1, "Error al obtener la ruta de red");
+		}
+	}
+}
